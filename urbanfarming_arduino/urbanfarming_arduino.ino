ef9207227fc0caebef16a19063d918d72e902ca6
huachen <huachenw24@gmail.com>
2021-09-28 00:40:00 +0800

    add in basic functions

--- urbanfarming_arduino/urbanfarming_arduino.ino
+++ urbanfarming_arduino/urbanfarming_arduino.ino
@@ -6,8 +6,25 @@
 #define FLOAT_1 2
 #define FLOAT_2 3
 #define FLOAT_3 4
+#define MAIN_PUMP 5
 
+const int readInterval = 30000;         // 30 sec
+const int postInterval = 600000;        // 10 min
+const int peristalticDuration = 1000;   // 1 sec
+const int pumpOnDuration = 300000;      // 5 min
+const int pumpOffDuration = 720000;     // 12 min
+const int BUFFER_SIZE = 32;
+char buf[BUFFER_SIZE];
+
+unsigned long currentMillis = 0;
+unsigned long previousReadMillis = 0;
+unsigned long previousPostMillis = 0;
+unsigned long previousPeristalticMillis = 0;
+unsigned long previousPumpMillis = 0;
+
+byte pumpState = LOW;
 float voltage,ecValue,temperature;
+byte contA = LOW, contB = LOW, contC = LOW;
 
 Adafruit_BME280 bme;
 DFRobot_EC ec;
@@ -22,35 +39,73 @@ void setup() {
 }
 
 void loop() {
-  temperature = bme.readTemperature();
-  voltage = analogRead(EC_PIN)/1024.0*5000;
-  ecValue = ec.readEC(voltage, temperature);
-  
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println("C");
-  
-  Serial.print("Humidity: ");
-  Serial.print(bme.readHumidity());
-  Serial.println("%");
-  
-  Serial.print("EC: ");
-  Serial.print(ecValue, 2);
-  Serial.println("ms/cm");
+  currentMillis = millis();
+  pump();
+  readData();
+  postData();
+  readCommand();
+}
 
-  if (digitalRead(FLOAT_1) == LOW) {
-    Serial.println("Container 1 low");
+void pump() {
+  if (pumpState == LOW) {
+    if (currentMillis - previousPumpMillis >= pumpOffDuration) {
+      pumpState == HIGH;
+      digitalWrite(MAIN_PUMP, pumpState);
+      previousPumpMillis += pumpOffDuration;
+    }
+  } else {
+    if (currentMillis - previousPumpMillis >= pumpOnDuration) {
+      pumpState == LOW;
+      digitalWrite(MAIN_PUMP, pumpState);
+      previousPumpMillis += pumpOnDuration;
+    }
   }
+}
 
-  if (digitalRead(FLOAT_2) == LOW) {
-    Serial.println("Container 2 low");
+void readData() {
+  if (currentMillis - previousReadMillis >= readInterval) {
+    temperature = bme.readTemperature();
+    voltage = analogRead(EC_PIN)/1024.0*5000;
+    ecValue = ec.readEC(voltage, temperature);
+    contA = digitalRead(FLOAT_1);
+    contB = digitalRead(FLOAT_2);
+    contC = digitalRead(FLOAT_3);
+    previousReadMillis += readInterval;
   }
+}
 
-  if (digitalRead(FLOAT_3) == LOW) {
-    Serial.println("Container 3 low");
-  }
+void postData() {
+  if (currentMillis - previousPostMillis >= postInterval) {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println("C");
+    
+    Serial.print("Humidity: ");
+    Serial.print(bme.readHumidity());
+    Serial.println("%");
+    
+    Serial.print("EC: ");
+    Serial.print(ecValue, 2);
+    Serial.println("ms/cm");
   
-  Serial.println();
-  delay(1000);
-  ec.calibration(voltage,temperature);
+    if (contA == LOW) {
+      Serial.println("Container A low");
+    }
+    if (contB == LOW) {
+      Serial.println("Container B low");
+    }
+    if (contC == LOW) {
+      Serial.println("Container C low");
+    }
+    
+    Serial.println();
+    previousPostMillis += postInterval;
+  }
+}
+
+void readCommand() {
+  if (Serial.available() >= BUFFER_SIZE) {
+    Serial.readBytes(buf, BUFFER_SIZE);
+    Serial.print(buf);
+  }
 }
