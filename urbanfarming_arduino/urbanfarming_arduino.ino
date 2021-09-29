795b901431f3034c82cca648444266007079d65f
huachen <huachenw24@gmail.com>
2021-09-29 11:10:17 +0800

    update 29 sep

--- urbanfarming_arduino/urbanfarming_arduino.ino
+++ urbanfarming_arduino/urbanfarming_arduino.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include "DFRobot_EC.h"
+#include <DFRobot_EC.h>
 
 #define EC_PIN A1
 #define FLOAT_1 2
@@ -8,13 +8,45 @@
 #define FLOAT_3 4
 #define MAIN_PUMP 5
 
-const int readInterval = 30000;         // 30 sec
-const int postInterval = 600000;        // 10 min
-const int peristalticDuration = 1000;   // 1 sec
-const int pumpOnDuration = 300000;      // 5 min
-const int pumpOffDuration = 720000;     // 12 min
-const int BUFFER_SIZE = 32;
-char buf[BUFFER_SIZE];
+struct __attribute__((__packed__)) sensorData {
+  float EC;
+  float pH;
+  float temp;
+  float humidity;
+  byte contA;
+  byte contB;
+  byte contC;
+};
+
+const int data_union_size = sizeof(sensorData);
+
+union dataPacket_t {
+  sensorData sensors;
+  byte byteArray[data_union_size];
+};
+
+dataPacket_t data;
+
+struct commandData {
+  byte pump;
+};
+
+const int command_union_size = sizeof(commandData);
+
+union commandPacket_t {
+  commandData commands;
+  byte byteArray[command_union_size];
+};
+
+commandPacket_t command;
+
+const long readInterval = 3000;         // 30 sec
+const long postInterval = 6000;        // 10 min
+const long peristalticDuration = 1000;   // 1 sec
+const long pumpOnDuration = 300000;      // 5 min
+const long pumpOffDuration = 720000;     // 12 min
+
+byte piCommand[command_union_size];
 
 unsigned long currentMillis = 0;
 unsigned long previousReadMillis = 0;
@@ -23,7 +55,7 @@ unsigned long previousPeristalticMillis = 0;
 unsigned long previousPumpMillis = 0;
 
 byte pumpState = LOW;
-float voltage,ecValue,temperature;
+float voltage,ecValue,temperature, humidity, pH;
 byte contA = LOW, contB = LOW, contC = LOW;
 
 Adafruit_BME280 bme;
@@ -36,6 +68,7 @@ void setup() {
   pinMode(FLOAT_1, INPUT_PULLUP);
   pinMode(FLOAT_2, INPUT_PULLUP);
   pinMode(FLOAT_3, INPUT_PULLUP);
+  pinMode(6, OUTPUT);
 }
 
 void loop() {
@@ -43,7 +76,7 @@ void loop() {
   pump();
   readData();
   postData();
-  readCommand();
+//  readCommand();
 }
 
 void pump() {
@@ -64,48 +97,66 @@ void pump() {
 
 void readData() {
   if (currentMillis - previousReadMillis >= readInterval) {
-    temperature = bme.readTemperature();
-    voltage = analogRead(EC_PIN)/1024.0*5000;
+//    temperature = bme.readTemperature();
+//    humidity = bme.readHumidity();
+//    voltage = analogRead(EC_PIN)/1024.0*5000;
+//    ecValue = ec.readEC(voltage, temperature);
+//    contA = digitalRead(FLOAT_1);
+//    contB = digitalRead(FLOAT_2);
+//    contC = digitalRead(FLOAT_3);
+    temperature = 25.5;
+    humidity = 70;
+    voltage = 3000;
+    pH = 6.5;
     ecValue = ec.readEC(voltage, temperature);
     contA = digitalRead(FLOAT_1);
     contB = digitalRead(FLOAT_2);
     contC = digitalRead(FLOAT_3);
+//    Serial.println(ecValue);
+//    Serial.println(temperature);
+//    Serial.println(humidity);
     previousReadMillis += readInterval;
   }
 }
 
 void postData() {
   if (currentMillis - previousPostMillis >= postInterval) {
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println("C");
-    
-    Serial.print("Humidity: ");
-    Serial.print(bme.readHumidity());
-    Serial.println("%");
-    
-    Serial.print("EC: ");
-    Serial.print(ecValue, 2);
-    Serial.println("ms/cm");
-  
-    if (contA == LOW) {
-      Serial.println("Container A low");
+    data.sensors.temp = temperature;
+    data.sensors.EC = ecValue;
+    data.sensors.humidity = humidity;
+    data.sensors.pH = pH;
+    data.sensors.contA = contA;
+    data.sensors.contB = contB;
+    data.sensors.contC = contC;
+
+    for (int i=0; i < data_union_size; i++) {
+      if (data.byteArray[i] <16) {
+        Serial.print(0);
+      }
+      Serial.print(data.byteArray[i], HEX);
+//      Serial.print("\t");
     }
-    if (contB == LOW) {
-      Serial.println("Container B low");
-    }
-    if (contC == LOW) {
-      Serial.println("Container C low");
-    }
+    Serial.print("\n");
     
-    Serial.println();
     previousPostMillis += postInterval;
   }
 }
 
 void readCommand() {
-  if (Serial.available() >= BUFFER_SIZE) {
-    Serial.readBytes(buf, BUFFER_SIZE);
-    Serial.print(buf);
+  if (Serial.available() >= command_union_size) {
+    for (byte n=0; n<command_union_size; n++) {
+      piCommand[n] = Serial.read();
+    }
+    for (byte n=0; n<command_union_size; n++) {
+      command.byteArray[n] = piCommand[n];
+    }
+    switch (command.commands.pump) {
+      case 1:
+        digitalWrite(6, HIGH);
+        break;
+      default:
+        digitalWrite(6, LOW);
+        break;
+    }
   }
 }
